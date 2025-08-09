0f5569e781a614723f774085a280b35d2399a037
Seju <126666482+Seju1033@users.noreply.github.com>
2025-08-09 23:15:50 +0530

    updated

---
+++ practice-3/letHoisting.js
@@ -0,0 +1,8 @@
+// console.log(a) it will throw an error ( can not access a brfore initializing)
+// console.log(b) 
+let a = 10
+console.log(a)
+const b = 20
+console.log(b)
+// let and const stored in a diffrent memory space /Script thats why when accessing before initializing 
+// throws an error
